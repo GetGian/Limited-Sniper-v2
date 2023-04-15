e8f79101ea7a251dc4b542a59b750c0cd7264d47
Gian! <79874148+GetGian@users.noreply.github.com>
2023-04-15 11:03:47 +0800

    Update update.py

--- scripts/update.py
+++ scripts/update.py
@@ -1,7 +1,7 @@
 import requests as r
 
 print("Checking for updates...")
-script = r.get("").text
+script = r.get("https://github.com/GetGian/Limited-Sniper-v2/blob/main/main.py").text
 with open("main.py", "r") as f:
    if f.read() != script:
         print("Updating...")
