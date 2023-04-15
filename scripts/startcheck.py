ac831560e7e75e627c4aaeef36e9da5ee524d88b
Gian! <79874148+GetGian@users.noreply.github.com>
2023-04-15 11:24:03 +0800

    Delete scripts directory

--- scripts/startcheck.py
+++
@@ -1,9 +0,0 @@
-import os
-
-if not os.path.exists("./config.json"):
-    print("Ensuring pip is installed")
-    os.system("python -m ensurepip --upgrade")
-
-    print("Installing required packages")
-    os.system("python -m pip install -r ./requirements.txt")
-    exit(0)
